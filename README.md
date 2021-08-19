4e84e7ec031b00eb026d6b297dccf934f5b21ea3
Weiyuan Jiang <weiyuan.jiang@nasa.gov>
2021-08-19 10:52:16 -0400

    deduce reference time and duration from template

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # OSSEtool
 produce data from nature runs and observation locations
+
+Requirement:
+ 1)module load python/GEOSpyD/Ana2019.03_py3.7
+
+
--- main.py
+++ main.py
@@ -18,16 +18,61 @@ from netCDF4 import Dataset
 from datetime import datetime
 from datetime import timedelta
 from dateutil.relativedelta import *
+import glob
 
 def get_file_name(ftpl, time) :
   return ftpl.format(y4=time.year, m2=str(time.month).zfill(2), \
                   d2 = str(time.day).zfill(2), h2=str(time.hour).zfill(2), \
                   mn2= str(time.minute).zfill(2), s2=str(time.second).zfill(2))
 
-def read_variables(fname, start_time, var_list, lats, lons, variables):
+def get_reference_time_duration(ftpl, start_time):
+   last_ = ftpl.rfind('/')
+   fpath = ftpl[:last_] if (last_ >=0) else '.'
+   fpath = get_file_name(fpath, start_time)
 
+   fname = ftpl[last_+1:]
+   wild  = fname.replace('{y4}','*').replace('{m2}','*').replace('{d2}','*').replace('{h2}','*').replace('{mn2}','*')
+   files = glob.glob(fpath+'/'+wild)
+
+   assert files, "cannot find files " + fpath+'/'+wild
+   k_year = fname.find('{y4}')
+   k_mon  = fname.find('{m2}')
+   k_day  = fname.find('{d2}') - 2
+   k_hour = fname.find('{h2}') - 4
+   k_min  = fname.find('{mn2}')- 6
+   k_sec  = fname.find('{s2}') - 9
+  
+   print(files[0]) 
+   last_ = files[0].rfind('/')
+   fname = files[0][last_+1:]
+
+   YYYY = fname[k_year:k_year+4] if k_year >=0 else '0000'
+   MM   = fname[k_mon:k_mon+2] if k_mon >=0 else '00'  
+   DD   = fname[k_day:k_day+2] if k_day >=0 else '00' 
+   HH   = fname[k_hour:k_hour+2] if k_hour >=0 else '00'
+   MN   = fname[k_min:k_min+2]   if k_min >=0 else '00' 
+   SS   = fname[k_sec:k_sec+2]   if k_sec >=0 else '00' 
+   reftime  = '-'.join((YYYY,MM,DD))+' '+":".join((HH,MN,SS))
+   ref_time = datetime.fromisoformat(reftime)
+   print("inside: ref_time ", ref_time)
+   duration = 0 
+   if len(files) >=2 :
+      last_ = files[1].rfind('/')
+      fname = files[1][last_+1:]
+      
+      YYYY = fname[k_year:k_year+4] if k_year >=0 else '0000'
+      MM   = fname[k_mon:k_mon+2] if k_mon >=0 else '00'  
+      DD   = fname[k_day:k_day+2] if k_day >=0 else '00' 
+      HH   = fname[k_hour:k_hour+2] if k_hour >=0 else '00'
+      MN   = fname[k_min:k_min+2]   if k_min >=0 else '00' 
+      SS   = fname[k_sec:k_sec+2]   if k_sec >=0 else '00' 
+      reftime1 = '-'.join((YYYY,MM,DD))+' '+":".join((HH,MN,SS))
+      ref_time1 = datetime.fromisoformat(reftime1)
+      duration = ref_time1 -ref_time
+   return ref_time, duration
+
+def read_variables(fname, start_time, var_list, lats, lons):
    # verify time and fname or time stamp in file
-
    fh = Dataset(fname,mode='r')
    n_level = fh.dimensions['lev'].size    
    n_lat = fh.dimensions['lat'].size
@@ -37,6 +82,7 @@ def read_variables(fname, start_time, var_list, lats, lons, variables):
   
    Is =((np.array(lats)+90.0 )/dlat).astype('int32')
    Js =((np.array(lons)+180.0)/dlon).astype('int32')
+   variables =[]
    for i in range(len(var_list)):
       var_name =  var_list[i]
       var_in = fh.variables[var_name][:, :, :, :]
@@ -44,6 +90,7 @@ def read_variables(fname, start_time, var_list, lats, lons, variables):
       for k in range(len(Is)):
         var[:,k] = var_in[0,:,Is[k],Js[k]]
       variables.append((var_name, n_level, var, fh.variables[var_name].__dict__))
+   return variables
 
 def write_variables(fname, start_time, variables, lats, lons, second_increase):
 
@@ -65,7 +112,7 @@ def write_variables(fname, start_time, variables, lats, lons, second_increase):
    lons_.long_name = "longitude" ;
    lons_.units = "degrees_east" ;
 
-   for i in range(len(var_list)) :
+   for i in range(len(variables)) :
       var_name = variables[i][0]
       value_ = fh.createVariable(var_name, np.float32, ('lev','index',))
       value_.setncatts(variables[i][3])
@@ -88,12 +135,15 @@ if __name__ == '__main__' :
    end_time   = datetime.fromisoformat(str(config['output']['end_time']))
    out_duration = timedelta(seconds = int(config['output']['duration']))
  
-   sat_reference_time = datetime.fromisoformat(str(config['locations']['reference_time']))
-   sat_duration = timedelta(seconds = int(config['locations']['duration'])) 
-
+   sat_reference_time, sat_duration = get_reference_time_duration(config['locations']['template'], start_time)
+   if sat_duration ==0 :
+      if 'duration' in config['locations']:
+         sat_duration = timedelta(seconds = int(config['locations']['duration'])) 
+      else:
+         assert false ,  "duration is needed when there is only one file"
    # get the first sat fname
    distance = (start_time - sat_reference_time)/sat_duration
-   sat_time = sat_reference_time + int(distance) * sat_duration + sat_duration/2
+   sat_time = sat_reference_time + int(distance) * sat_duration
    start_sat_fname = get_file_name(config['locations']['template'], sat_time)
 
    lats = []
@@ -135,16 +185,21 @@ if __name__ == '__main__' :
          # read variables 
          for fields in model_fields :
            for var_names, collection in fields.items():
-              reference_time_ = str(config['collections'][collection['collection']]['reference_time'])
-              frequency_      = int(config['collections'][collection['collection']]['frequency'])
-              model_reference_time = datetime.fromisoformat(reference_time_)
-              model_frequency      = timedelta(seconds = frequency_)
+              ftpl  = config['collections'][collection['collection']]['template']
+              model_reference_time, model_frequency = get_reference_time_duration(ftpl, start_time)
+              if (model_frequency ==0):
+                 if 'duration' in config['collections'][collection['collection']]:
+                    model_ferquency = timedelta(seconds = int(config['collections'][collection['collection']]['duration']))
+                 else:
+                    assert false, "duration is needed in " + collection['collection']
               # by default, find the nearest model file
               distance   = (start_time - model_reference_time)/model_frequency 
               model_time = model_reference_time + int(distance) * model_frequency
               model_fname = get_file_name(config['collections'][collection['collection']]['template'], model_time)
               var_list = var_names.split()
-              read_variables(model_fname, model_time, var_list, lats, lons, variables)
+              collection_vars = read_variables(model_fname, model_time, var_list, lats, lons)
+              for var in collection_vars:
+                 variables.append(var)
          # write out variables
          out_fname = get_file_name(config['output']['template'], start_time)
          write_variables(out_fname, start_time, variables, lats,lons, second_increase)
